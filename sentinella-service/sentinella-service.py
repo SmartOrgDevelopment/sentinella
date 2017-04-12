f11c2a7b6d978dfd88cdb44ad51ae6a2ce960868
FanSmartOrg <fyu@smartorg.com>
2017-04-12 15:08:55 -0700

    Create a api to get travis report.

--- sentinella-service/sentinella-service.py
+++ sentinella-service/sentinella-service.py
@@ -1,11 +1,45 @@
-import json
-import sys
+import os
 
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+__TRAVIS_OUTPUT_PASSED = "travis_output_passed.json"
+__TRAVIS_OUTPUT_FAILED = "travis_output_failed.json"
+
+PASSED = "passed"
+FAILED = "failed"
+ERROR = "error"
+
+
+@app.route("/")
+def info():
+    return "This is the API of SmartOrg Sentinella"
+
+
+@app.route("/get-status", methods=["GET"])
+def get_status():
+    if os.path.exists(__TRAVIS_OUTPUT_PASSED):
+        return PASSED
+    elif os.path.exists(__TRAVIS_OUTPUT_FAILED):
+        return FAILED
+    else:
+        return ERROR
+
+
+@app.route("/get-report", methods=["GET"])
+def get_travis_report():
+    if os.path.exists(__TRAVIS_OUTPUT_PASSED):
+        with open(__TRAVIS_OUTPUT_PASSED) as data_file:
+            return data_file.read()
+    elif os.path.exists(__TRAVIS_OUTPUT_FAILED):
+        with open(__TRAVIS_OUTPUT_FAILED) as data_file:
+            return data_file.read()
+    else:
+        return ERROR
+
+
+if __name__ == "__main__":
     app.run()
 
 
